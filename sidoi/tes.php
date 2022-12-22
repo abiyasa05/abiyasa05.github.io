df702a6b060b3a7af47f5d06193af7d272fbdd0d
abiyasa05 <abiyasap24@gmail.com>
2022-12-22 10:43:54 +0700

    tes

---
+++ sidoi/tes.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tes</title>
+</head>
+
+<body>
+    <p>Hello World</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Nulla autem aliquam ratione rem obcaecati quibusdam corporis, deleniti veritatis alias sunt repudiandae.
+        Laborum exercitationem modi minima ex. Voluptatem maxime voluptates praesentium.</p>
+</body>
+
+</html>
