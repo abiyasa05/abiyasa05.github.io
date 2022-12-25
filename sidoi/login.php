9ead92713a9710c275eee3ba05f925558b0a465e
Lenka <falendikapratama@gmail.com>
2022-12-25 14:32:33 +0700

    Update login and register
    Perubahan pada login dan register beserta penambahan bootstrapnya.

--- sidoi/login.php
+++ sidoi/login.php
@@ -1,34 +1,120 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <title>Ez Game - Login</title>
-
-    <link rel='stylesheet' type='text/css' href='main.css'>
+  <title>Sidoi - Login Page</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="login/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="login/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="login/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/css/util.css">
+	<link rel="stylesheet" type="text/css" href="login/css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
-    <div class="wrap blurbawah">
-    	<div id="kontenlogin">
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('login/images/bg-01.jpg');">
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+				<form class="login100-form validate-form" method="POST" action="proses_login.php">
+					<span class="login100-form-title p-b-49">
+						Halaman Login
+					</span>
 
-        <!-- Judul -->
-        <h2 class="aktif"> Login </h2>
+					<div class="wrap-input100 validate-input m-b-23" data-validate = "Email tidak boleh kosong">
+						<span class="label-input100">Email</span>
+						<input class="input100" type="text" name="email_pelanggan" placeholder="Masukkan email">
+						<span class="focus-input100" data-symbol="&#xf206;"></span>
+					</div>
 
-        <!-- Login -->
-        <form method="post" action="proses_login.php">
-          <input type="text" id="login" class="blur pertama" name="email_pelanggan" placeholder="Email">
-          <input type="password" id="password" class="blur kedua" name="password_pelanggan" placeholder="Password">
-          <br><br>
-          <input type="submit" class="blur ketiga" name="submit" value="Login">
-        </form>
+					<div class="wrap-input100 validate-input" data-validate="Password tidak boleh kosong">
+						<span class="label-input100">Password</span>
+						<input class="input100" type="password" name="password_pelanggan" placeholder="Masukkan password">
+						<span class="focus-input100" data-symbol="&#xf190;"></span>
+					</div>
+					
+					<div class="text-right p-t-8 p-b-31">
+						
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn" type="submit" name="submit">
+								Login
+							</button>
+						</div>
+					</div>
 
-        <!-- Lupa password -->
-        <div id="footer">
-          <a class="garisbawah" href="register.php"> Belum punya akun? Daftar </a>
-        </div>
-      </div>
-    </div>
+					<div class="txt1 text-center p-t-35 p-b-20">
+						<span>
+							Login menggunakan
+						</span>
+					</div>
+
+					<div class="flex-c-m">
+						<a href="#" class="login100-social-item bg1">
+							<i class="fa fa-facebook"></i>
+						</a>
+
+						<a href="#" class="login100-social-item bg2">
+							<i class="fa fa-twitter"></i>
+						</a>
+
+						<a href="#" class="login100-social-item bg3">
+							<i class="fa fa-google"></i>
+						</a>
+					</div>
+
+					<div class="flex-col-c p-t-30">
+						<span class="txt1 p-b-17">
+							Belum punya akun?
+						</span>
+
+						<a href="register.php" class="txt1">
+							Register disini!
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
 </body>
 </html>
--- sidoi/register.php
+++ sidoi/register.php
@@ -1,37 +1,116 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <title>Ez Game - Register</title>
-    
-    <link rel='stylesheet' type='text/css' href='main.css'>
+  <title>Sidoi - Register Page</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="login/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="login/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="login/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="login/css/util.css">
+	<link rel="stylesheet" type="text/css" href="login/css/main.css">
+<!--===============================================================================================-->
 </head>
-<body class="back">
-    <div class="wrap blurbawah">
-	<div id="kontenregister">
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('login/images/bg-01.jpg');">
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+				<form class="login100-form validate-form" method="POST" action="proses_registrasi.php">
+					<span class="login100-form-title p-b-49">
+						Halaman Register
+					</span>
 
-    <!-- Judul -->
-    <h2 class="aktif"> Register </h2>
+                    <div class="wrap-input100 validate-input m-b-23" data-validate = "Nama lengkap tidak boleh kosong">
+						<span class="label-input100">Nama Lengkap</span>
+						<input class="input100" type="text" name="nama_pelanggan" placeholder="Masukkan nama lengkap">
+						<span class="focus-input100" data-symbol="&#xf206;"></span>
+					</div>
 
-    <!-- Login -->
-    <form method="post" action="proses_registrasi.php">
-      <input type="text" id="nama" class="blur pertama" name="nama_pelanggan" placeholder="Nama Lengkap">
-      <input type="text" id="daftar" class="blur kedua" name="email_pelanggan" placeholder="Email">
-      <input type="number" id="telepon" class="blur ketiga" name="telepon_pelanggan" placeholder="Nomor Telepon">
-      <input type="password" id="password" class="blur keempat" name="password_pelanggan" placeholder="Password">
-      <input type="password" id="password" class="blur kelima" name="ulang_password" placeholder="Ulangi Password">
-      <br><br>
-      <input type="submit" class="blur keenam" name="kirim" value="Register">
-    </form>
+					<div class="wrap-input100 validate-input m-b-23" data-validate = "email tidak boleh kosong">
+						<span class="label-input100">Email</span>
+						<input class="input100" type="text" name="email_pelanggan" placeholder="Masukkan email">
+						<span class="focus-input100" data-symbol="&#xf194;"></span>
+					</div>
 
-    <!-- Lupa password -->
-    <div id="footer">
-      <a class="garisbawah" href="login.php"> Sudah punya akun? Login </a>
-    </div>
-  </div>
-</div>
+                    <div class="wrap-input100 validate-input m-b-23" data-validate = "Nomor telepon tidak boleh kosong">
+						<span class="label-input100">Nomor Telpon</span>
+						<input class="input100" type="number" name="telepon_pelanggan" placeholder="Masukkan nomor telepon">
+						<span class="focus-input100" data-symbol="&#xf170;"></span>
+					</div>
+                    
+					<div class="wrap-input100 validate-input m-b-23" data-validate="Password tidak boleh kosong">
+						<span class="label-input100">Password</span>
+						<input class="input100" type="password" name="password_pelanggan" placeholder="Masukkan password">
+						<span class="focus-input100" data-symbol="&#xf190;"></span>
+					</div>
+					
+                    <div class="wrap-input100 validate-input" data-validate="Konfirmasi password tidak boleh kosong">
+						<span class="label-input100">Konfirmasi Password</span>
+						<input class="input100" type="password" name="ulang_password" placeholder="Masukkan konfirmasi password">
+						<span class="focus-input100" data-symbol="&#xf190;"></span>
+					</div>
+
+					<div class="text-right p-t-8 p-b-31">
+						
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+                            <input id="jkl" class="login100-form-btn" type="submit" name="kirim" value="Register">
+						</div>
+					</div>
+
+					<div class="flex-col-c p-t-30">
+						<span class="txt1 p-b-17">
+							Sudah punya akun?
+						</span>
+
+						<a href="login.php" class="txt1">
+							Login disini!
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
 </body>
 </html>
