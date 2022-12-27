a5a6d8b3a1ec0042e806e0404a86271d2dd3d297
abiyasa05 <abiyasap24@gmail.com>
2022-12-27 16:06:18 +0700

    Tambahan id carousel

--- sidoi/index2.php
+++ sidoi/index2.php
@@ -293,7 +293,7 @@ $em = $ambil->fetch_assoc();
 
 		<hr color="black">
 
-		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+		<div id="carouselExampleControls2" class="carousel slide" data-ride="carousel">
 			<div class="carousel-inner">
 				<div class="carousel-item active">
 					<div class="cards-wrapper">
@@ -380,11 +380,11 @@ $em = $ambil->fetch_assoc();
 					</div>
 				</div>
 			</div>
-			<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			<a class="carousel-control-prev" href="#carouselExampleControls2" role="button" data-slide="prev">
 				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 				<span class="sr-only">Previous</span>
 			</a>
-			<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			<a class="carousel-control-next" href="#carouselExampleControls2" role="button" data-slide="next">
 				<span class="carousel-control-next-icon" aria-hidden="true"></span>
 				<span class="sr-only">Next</span>
 			</a>
