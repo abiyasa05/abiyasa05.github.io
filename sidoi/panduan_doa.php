e3aa19535d501f2c00f07cef18efc0c96f5ccfdf
abiyasa05 <abiyasap24@gmail.com>
2022-12-29 11:08:37 +0700

    margin top panduan doa

--- sidoi/panduan_doa.php
+++ sidoi/panduan_doa.php
@@ -71,7 +71,7 @@ $em = $ambil->fetch_assoc();
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="asset/css/bootstrap.min.css">
-    
+
     <!-- Style -->
     <link rel="stylesheet" href="asset/css/style.css">
 
@@ -92,51 +92,51 @@ $em = $ambil->fetch_assoc();
 <body>
     <!--Navbar -->
     <div class="site-mobile-menu site-navbar-target">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
+        <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+                <span class="icon-close2 js-menu-toggle"></span>
+            </div>
         </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
+        <div class="site-mobile-menu-body"></div>
     </div>
-   
-    
+
+
     <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
 
-      <div class="container">
-        <div class="row align-items-center">
-          
-          <div class="col-6 col-xl-2">
-            <h1 class="mb-0 site-logo"><a href="index2.php">SI - DO'I</a></h1>
-          </div>
+        <div class="container">
+            <div class="row align-items-center">
 
-          <div class="col-12 col-md-10 d-none d-xl-block">
-            <nav class="site-navigation position-relative text-right" role="navigation">
+                <div class="col-6 col-xl-2">
+                    <h1 class="mb-0 site-logo"><a href="index2.php">SI - DO'I</a></h1>
+                </div>
 
-              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                <li><a href="index2.php" class="nav-link">Tour Builder</a></li>
-                <li><a href="paket_ziarah.php" class="nav-link">Paket Ziarah</a></li>
-				<li><a href="panduan_doa.php" class="nav-link active">Panduan Doa</a></li>
-				<li><a href="history_pesanan.php" class="nav-link">History Pesanan</a></li>
-				<li><a href="wishlist.php" class="nav-link" style="margin-left: 50px;"><i class="icon-bookmark"></i></a></li>
-				<li><a href="keranjang.php" class="nav-link"><i class="icon-shopping-cart"></i></a></li>
-				<li class="has-children">
-                  <a href="#" class="nav-link"><?php echo $_SESSION["pelanggan"]["nama_pelanggan"] ?></a>
-                  <ul class="dropdown">
-                    <li><a href="profil.php?email=<?php echo $_SESSION["pelanggan"]["email_pelanggan"]; ?>" class="nav-link">Profil</a></li>
-                    <li><a href="logout.php" class="nav-link">Logout</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
+                <div class="col-12 col-md-10 d-none d-xl-block">
+                    <nav class="site-navigation position-relative text-right" role="navigation">
+
+                        <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                            <li><a href="index2.php" class="nav-link">Tour Builder</a></li>
+                            <li><a href="paket_ziarah.php" class="nav-link">Paket Ziarah</a></li>
+                            <li><a href="panduan_doa.php" class="nav-link active">Panduan Doa</a></li>
+                            <li><a href="history_pesanan.php" class="nav-link">History Pesanan</a></li>
+                            <li><a href="wishlist.php" class="nav-link" style="margin-left: 50px;"><i class="icon-bookmark"></i></a></li>
+                            <li><a href="keranjang.php" class="nav-link"><i class="icon-shopping-cart"></i></a></li>
+                            <li class="has-children">
+                                <a href="#" class="nav-link"><?php echo $_SESSION["pelanggan"]["nama_pelanggan"] ?></a>
+                                <ul class="dropdown">
+                                    <li><a href="profil.php?email=<?php echo $_SESSION["pelanggan"]["email_pelanggan"]; ?>" class="nav-link">Profil</a></li>
+                                    <li><a href="logout.php" class="nav-link">Logout</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
 
 
-          <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a></div>
+                <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a></div>
 
+            </div>
         </div>
-      </div>
-      
+
     </header>
 
     <div class="hero" style="height: 400px; background-image: url('asset/images/new3.png');"></div>
@@ -222,7 +222,7 @@ $em = $ambil->fetch_assoc();
     <!-- /konten -->
 
     <!-- Footer -->
-    <footer class="page-footer bg-dark font-small stylish-color-dark pt-4" style="color: white">
+    <footer class="page-footer bg-dark font-small stylish-color-dark pt-4" style="color: white; margin-top: 130px;">
 
         <!-- Footer Links -->
         <div class="container text-center text-md-left">
